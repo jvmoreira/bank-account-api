fb76754bc77d09c7f65db555b2622c7632b2250e
João Vitor Moreira <themoreira.jvm@gmail.com>
2021-03-22 23:10:40 -0300

    feat: introduce express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello World');
+import express, { Request, Response } from 'express';
+
+const app = express();
+app.use(express.json());
+const PORT = 8000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({ message: 'API is up and running' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at https://localhost:${PORT}`);
+});
